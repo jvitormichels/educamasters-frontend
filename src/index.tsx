6a1bf5ed9f3d869cf0bd15552e392c9b05f8c940
jvitormichels <jvitormichels@gmail.com>
2024-11-30 16:00:17 -0300

    feat: desabilitar React Strict Mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global.css';
 import reportWebVitals from './reportWebVitals';
@@ -6,15 +5,17 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes';
 
+import { Provider } from './components/ui/provider';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <Provider>
       <AppRoutes />
-    </BrowserRouter>
-  </React.StrictMode>
+    </Provider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
