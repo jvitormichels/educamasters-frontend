1afa7e56a13d12fdfd85f1f7b122edfe33d5ddf8
jvitormichels <jvitormichels@gmail.com>
2024-12-01 21:56:48 -0300

    feat: imagem de placeholder para cursos sem thumbnail

--- src/components/course-list/CourseCard.tsx
+++ src/components/course-list/CourseCard.tsx
@@ -4,10 +4,12 @@ import { Course } from "@/types/course"
 import { Image } from "@chakra-ui/react"
 
 const CourseCard = (course: Course) => {
+  const placeholderImage = "https://via.placeholder.com/750x422?text=No+Image"
+
   return (
     <Card.Root maxW="sm" overflow="hidden">
       <Image
-        src={ course.thumbnail }
+        src={ course.thumbnail || placeholderImage }
         alt={ course.name }
       />
       <Card.Body gap="2">
